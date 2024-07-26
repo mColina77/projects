f67f69376c66736f92dc34a225d9fedd797177b9
mColina77 <111794219+mColina77@users.noreply.github.com>
2024-07-25 21:41:51 -0300

    primer commit

---
+++ 00-hola-mundo/src/App.jsx
@@ -0,0 +1,43 @@
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard.jsx'
+
+const users = [
+  {
+    userName: 'midudev',
+    name: 'Miguel Ángel Durán',
+    isFollowing: true
+  },
+  {
+    userName: 'pheralb',
+    name: 'Pablo Hernández',
+    isFollowing: false
+  },
+  {
+    userName: 'mcolina77',
+    name: 'Mauricio Colina',
+    isFollowing: true
+  },
+  {
+    userName: 'TMChein',
+    name: 'Tomas Chein',
+    isFollowing: false
+  }
+]
+
+export function App () {
+  return (
+    <section className='App'>
+      {
+        users.map(({ userName, name, isFollowing }) => (
+          <TwitterFollowCard
+            key={userName}
+            userName={userName}
+            initialIsFollowing={isFollowing}
+          >
+            {name}
+          </TwitterFollowCard>
+        ))
+      }
+    </section>
+  )
+}
